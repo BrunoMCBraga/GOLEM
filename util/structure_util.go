604c2285bb9a838d370ce8306d7fabd90b17fc73
Bruno Braga (brubraga) <bruno.mh.braga@gmail.com>
2021-01-30 16:42:28 +0100

    Changed list split

--- util/structure_util.go
+++ util/structure_util.go
@@ -1,5 +1,42 @@
 package util
 
+func SplitSliceIntoSubslices(slice []string, numberOfSlices int) []interface{} {
+
+	subSlices := make([]interface{}, 0)
+	sliceLength := len(slice) / numberOfSlices //3/2 = 1
+	sizeOfLastSlice := len(slice) % numberOfSlices
+	lastSubsliceIndex := 0
+
+	if numberOfSlices > len(slice) {
+		for _, sliceElement := range slice {
+			subSlice := make([]string, 0)
+			subSlice = append(subSlice, sliceElement)
+			subSlices = append(subSlices, subSlice)
+		}
+	} else {
+
+		for lastSubsliceIndex = 0; lastSubsliceIndex < numberOfSlices; lastSubsliceIndex++ {
+			subSlice := make([]string, 0)
+			for i := 0; i < sliceLength; i++ {
+				subSlice = append(subSlice, slice[lastSubsliceIndex*sliceLength+i])
+			}
+			subSlices = append(subSlices, subSlice)
+		}
+
+		remainingSlice := slice[lastSubsliceIndex:]
+
+		if sizeOfLastSlice > 0 {
+			for remainingSliceIndex, remainingSliceElement := range remainingSlice {
+				subSlices[remainingSliceIndex] = append(subSlices[remainingSliceIndex].([]string), remainingSliceElement)
+			}
+		}
+	}
+
+	return subSlices
+}
+
+/*
+//Old and wrong...
 func SplitSliceIntoSubslices(slice []string, subSlicesMaximumSize int) []interface{} {
 
 	subSlices := make([]interface{}, 0)
@@ -25,3 +62,4 @@ func SplitSliceIntoSubslices(slice []string, subSlicesMaximumSize int) []interfa
 
 	return subSlices
 }
+*/
